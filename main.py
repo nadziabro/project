cb3f504651a218930fea47108827d6c21007c87e
nadziabro <nadziabro0108@gmail.com>
2026-06-14 13:18:56 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,28 +2,47 @@ import telebot
 from telebot import types
 bot = telebot.TeleBot('8685421260:AAGRS3FfkLvB9CZZbxbtOpvV-dAKXhtd6TE')
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=2)
-    games = types.KeyboardButton('Создание игр')
-    mobile_app = types.KeybboadButton('Мобильная разроботка')
-    computer_app = types.KeyboardButton('ИРазроботчик софта')
-    data = types.KeyboardButton('Дата аналитика')
-    web_app = types.KeyboardButton('Веб разроботка')
-    artificial_intellegence = types.KeyboardButton('Разроботка искуственного интелекта')
-    keyboard.add(games,mobile_app,computer_app,data,web_app,artificial_intellegence)
+   keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=2)
+   game_dev = types.KeyboardButton('Game developer')
+   ai_dev = types.KeyboardButton('Ai development on python')
+   software_dev = types.KeyboardButton('Software development')
+   web_dev = types.KeyboardButton('Web development')
+   data_dev = types.KeyboardButton('DataScience development')
+   keyboard.add(game_dev,ai_dev,software_dev,web_dev,data_dev)
+   send_message = f"<b>Hello my dear</b>\n which type of quiz you want to solve?"
+   bot.send_message(message.chat.id,send_message,parse_mode='html',reply_markup=keyboard)
+@bot.message_handler(content_types=['text'])
+def mess(message):
+   if message.text =='Game developer':
+       markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+       btn1 = types.KeyboardButton('For mobile devices')
+       btn2 = types.KeyboardButton('PlayStation')
+       btn3 = types.KeyboardButton('Laptops/PC')
+       btn4 = types.KeyboardButton('Web-games')
+       btn5 = types.KeyboardButton('Start test again')
+       markup.add(btn1,btn2,btn3,btn3,btn4,btn5)
+       final_message = f"<b>Cool!!</b>\n The game development is best choise\n which platform for you want to develop? "
+       bot.send_message(message.chat.id,final_message,parse_mode='html',reply_markup=markup)
+   elif message.text == 'For mobile devices':
+       markup = types.InlineKeyboardMarkup()
+       unity = types.InlineKeyboardButton("Look course on Unity",url="https://learn.unity.com/")
+       markup.add(unity)
+       final_message=f"For mobile app development we actualy use the Unity platform"
+       bot.send_message(message.chat.id,final_message,parse_mode='html',reply_markup=markup)
+   elif message.text == 'Start test again':
+       send_mess = 'You wanna to start  from scrath again?/Choose another program'
+       keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=2)
+       game_dev = types.KeyboardButton('Game developer')
+       ai_dev = types.KeyboardButton('Ai development on python')
+       software_dev = types.KeyboardButton('Software development')
+       web_dev = types.KeyboardButton('Web development')
+       data_dev = types.KeyboardButton('DataScience development')
+       keyboard.add(game_dev,ai_dev,software_dev,web_dev,data_dev)
+       bot.send_message(message.chat.id,send_message,parse_mode='html',reply_markup=keyboard)
+    
+#python main.py
 
-    send_msg = f"<b>Привет!</b> \n Какой именно курс или направление тебя интересует?"
-    bot.send_message(message.chat.id, send_msg, parse_mode="html",reply_markup=keyboard)
-bot.polling(none_stop=True)
-
-
-@bot.message_handler(content_types==['text'])
-def  game_msg(message):
-    if message.text=='Создвние игр':
-        murcup = types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=2)
-        btn1=types.KeyboardButton('Под мобильные телефоны')
-        btn2=types.KeyboardButton('Комьютеры и консоли')
-        btn3=types.KeyboardButton('Виртуальная реальность')
-        btn4=types.KeyboardButton('Браузерская игра')
-        btn5=types.KeyboardButton('Вернуться в начальное меню')
+bot.infinity_polling()
